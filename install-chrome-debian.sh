e677bd691e338f8cf91537984cceca6eb8a1bb68
Karel Zimmer <karel.zimmer@gmail.com>
2022-03-27 21:50:06 +0200

    Refactoring

---
+++ install-chrome-debian.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Zie ook ~/ansible/install-chrome-debian.yml
+
+# FOUT
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget --no-verbose --output-document=- https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install --yes google-chrome-stable
+
+# GOED
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget --no-verbose --output-document=- https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor --yes --output=/usr/share/keyrings/google-chrome.gpg
+sudo apt-get update
+sudo apt-get install --yes google-chrome-stable
+sudo apt-key del 7FAC5991 D38B4796
+sudo rm --force /etc/apt/trusted.gpg.d/google-chrome*
