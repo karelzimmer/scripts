9985e38378debfd66c5888e0651943f376363391
Karel Zimmer <karel.zimmer@gmail.com>
2022-05-01 09:41:37 +0200

    Oef. NOVI

---
+++ bash/machtsverheffen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+# NOVI: Datastructuren en Algoritmen: Dynamic Programming en Recursion
+# Machtsverheffen, bereken 5^4 zonder machtsverheffen te gebruiken.
+
+# Dit is geen recursie, maar iteratie :-( !
+
+grondtal=5
+exponent=4
+result=1
+
+macht () 
+{
+    result=$((result * grondtal))
+}
+
+for i in $(seq 1 $exponent); do
+# for (( i=0; i<$exponent; i++ )); do
+    echo "5^$i"
+    macht
+done 
+
+echo "$result"
+exit 0
