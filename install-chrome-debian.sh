e5476025958b0004bd2e7408e35a7e363d8793c2
Karel Zimmer <karel.zimmer@gmail.com>
2022-05-16 15:23:12 +0200

    Add OpenPGP repository signing key because apt-key is deprecated

--- install-chrome-debian.sh
+++ install-chrome-debian.sh
@@ -2,13 +2,19 @@
 
 # Zie ook ~/ansible/install-chrome-debian.yml
 
-# FOUT
+# FOUT (key in /etc/apt/trusted.gpg)
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 wget --no-verbose --output-document=- https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install --yes google-chrome-stable
 
-# GOED
+# BETER (key in /etc/apt/trusted.gpg.d/)
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget --no-verbose --output-document=- https://dl.google.com/linux/linux_signing_key.pub | sudo sudo tee /etc/apt/trusted.gpg.d/google-chrome.asc
+sudo apt-get update
+sudo apt-get install --yes google-chrome-stable
+
+# GOED (signed-by toegevoegd aan source.list en key in /usr/share/keyrings/)
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 wget --no-verbose --output-document=- https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor --yes --output=/usr/share/keyrings/google-chrome.gpg
 sudo apt-get update
