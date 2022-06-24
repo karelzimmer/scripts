44675cdd418d3624fcb69bd1d1c2cd6094490bc2
Karel Zimmer <karel.zimmer@gmail.com>
2022-06-24 08:10:09 +0200

    Refactoring

--- bundle.sh
+++ bundle.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
-#############################################################################
-# Bestand:   bundle.sh                                                      #
-# Doel:      Script voor het bundelen van bestanden tot een shell-archief.  #
-# Gebruik:   ./bundle.sh [bestand]... > BESTAND                             #
-#            Bundelen:  $ ./bundle *.sh > scripts.shar                      #
-#            Uitpakken: $ bash scripts.shar                                 #
-# Gebruikt:  -                                                              #
-# Auteur:    Mark G. Sobell, A Practical Guide to Ubuntu Linux.             #
-#            Karel Zimmer (http://karelzimmer.nl, info@karelzimmer.nl)      #
-# ------------------------------------------------------------------------- #
-# Auteursrecht © 2011-2015 Karel Zimmer.                                    #
-#                                                                           #
-# Dit programma is vrije software: u mag het herdistribueren en/of wijzigen #
-# onder de voorwaarden van de GNU Algemene Publieke Licentie zoals          #
-# gepubliceerd door de Free Software Foundation, onder versie 3 van de      #
-# Licentie of (naar Uw keuze) elke latere versie.                           #
-#                                                                           #
-# Dit programma is gedistribueerd in de hoop dat het nuttig zal zijn maar   #
-# ZONDER ENIGE GARANTIE; zelfs zonder de impliciete garanties die           #
-# GEBRUIKELIJK ZIJN IN DE HANDEL of voor BRUIKBAARHEID VOOR EEN SPECIFIEK   #
-# DOEL.  Zie de GNU Algemene Publieke Licentie voor meer details.           #
-#                                                                           #
-# U hoort een kopie van de GNU Algemene Publieke Licentie te hebben         #
-# ontvangen samen met dit programma. Als dat niet het geval is, zie         #
-# http://www.gnu.org/licenses/.                                             #
-# ------------------------------------------------------------------------- #
-# Versies:      1.0.0   2011-01-28  Eerste versie.                          #
-#############################################################################
-readonly VERSION_NUMBER=1.3.1
-readonly RELEASE_DATE=2015-02-14
+###############################################################################
+# Bestand:   bundle.sh                                                      
+# Doel:      Script voor het bundelen van bestanden tot een shell-archief.  
+# Gebruik:   ./bundle.sh [bestand]... > BESTAND                             
+#            Bundelen:  $ ./bundle *.sh > scripts.shar                      
+#            Uitpakken: $ bash scripts.shar                                 
+# Gebruikt:  -                                                              
+# Auteur:    Mark G. Sobell, A Practical Guide to Ubuntu Linux.             
+#            Karel Zimmer (http://karelzimmer.nl, info@karelzimmer.nl)      
+# ----------------------------------------------------------------------------- 
+# Auteursrecht © 2011-2015 Karel Zimmer.                                    
+# ----------------------------------------------------------------------------- 
+# Versies:      1.0.0   2011-01-28  Eerste versie.                          
+###############################################################################
 
 # init_script
 echo '# Pak dit bestand uit met bash. Voorbeeld: $ bash bestand.shar'
@@ -52,5 +36,3 @@ done
 # afsl_script
 echo '# Einde bestand.'
 exit 0
-
-# Einde shell-archief.
